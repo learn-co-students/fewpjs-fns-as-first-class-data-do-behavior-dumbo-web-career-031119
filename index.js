6eff34240e1075164c4ccf67dac6a41270be32ee
beschwa <naraiswar@gmail.com>
2019-04-25 11:58:24 -0400

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+const greet = (time) => {
+	let newTime = parseInt(time.replace(":",""))
+	if (newTime < 1200) {
+		return 'Good Morning'
+	} else if (newTime > 1200 && newTime < 1700) {
+		return 'Good Afternoon'
+	} else {
+		return 'Good Evening'
+	}
+}
+
+const displayMessage = (message) => {
+	document.querySelector('#greeting').textContent = `${message}`
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
