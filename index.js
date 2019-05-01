08510e661acd0bc46a044c11170fa0346e34c02e
Miguel601 <henriquez.miguel@outlook.com>
2019-05-01 14:31:08 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+var d = new Date();
+var time = d.getHours();
+
+if (time < 12) {
+  document.write("<b>Good Morning!</b>");
+}
+if (time > 12) {
+  document.write("<b>Good Afternoon!</b>");
+}
+if (time == 12) {
+  document.write("<b>Go eat lunch!</b>");
+}
+}
 /* Write your implementation of displayMessage() */
+function displayMessage()
